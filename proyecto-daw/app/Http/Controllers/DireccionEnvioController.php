4e3190267ba82d58d761e9a9738a84ce54a1570d
raulitorbk23 <raulitorbk23@gmail.com>
2024-05-27 08:29:41 +0200

    26-05

---
+++ proyecto-daw/app/Http/Controllers/DireccionEnvioController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\DireccionEnvio;
+use App\Http\Requests\StoreDireccionEnvioRequest;
+use App\Http\Requests\UpdateDireccionEnvioRequest;
+
+class DireccionEnvioController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreDireccionEnvioRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(DireccionEnvio $direccionEnvio)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(DireccionEnvio $direccionEnvio)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateDireccionEnvioRequest $request, DireccionEnvio $direccionEnvio)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(DireccionEnvio $direccionEnvio)
+    {
+        //
+    }
+}
