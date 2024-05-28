0ce136d79183e6410f8815fed15eabda96f746f8
raulitorbk23 <raulitorbk23@gmail.com>
2024-05-28 08:15:52 +0200

    implementando la logica de base de datos

---
+++ proyecto-daw/app/Http/Controllers/UsuarioDireccionController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Usuario_Direccion;
+use App\Http\Requests\StoreUsuario_DireccionRequest;
+use App\Http\Requests\UpdateUsuario_DireccionRequest;
+
+class UsuarioDireccionController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreUsuario_DireccionRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Usuario_Direccion $usuario_Direccion)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Usuario_Direccion $usuario_Direccion)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateUsuario_DireccionRequest $request, Usuario_Direccion $usuario_Direccion)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Usuario_Direccion $usuario_Direccion)
+    {
+        //
+    }
+}
---
+++ proyecto-daw/database/seeders/UsuarioTarjetaSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UsuarioTarjetaSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+    }
+}
